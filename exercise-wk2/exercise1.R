86b58fcdc223f74d5e896805b8acd9332a1d7ddb
daisythemaster <duy29@uw.edu>
2018-01-16 17:21:45 -0800

    submit the exercises

--- exercise-wk2/exercise1.R
+++ exercise-wk2/exercise1.R
@@ -9,7 +9,9 @@ my.vector <- c(my.num, initials)
 vector.sum <- sum(my.vector)
 
 # Describe why this doesn't work: 
-# 
+# when my.num and initials are combined, it 
+# returns a character to my.vector which is
+# not a numerical value to use sum()
 
 install.packages("stringr")
 
@@ -18,21 +20,33 @@ my.line <- "Hey, hey, this is the library"
 print(str_length(my.line))
 
 # Describe why this doesn't work: 
-# 
+# because we need to use the library(’stringr’)
+# to get the access to the function str_length()
 
 said.the.famous <- paste(my.line, " - ", initial)
 
 # Describe why this doesn't work: 
-# 
+# there is no variable called initial created
+# so we can't use it inside the function
 
 
 ## Part 2 - Vector and function practice
 
 # Make a vector and use typeof to check what type R considers it to be
 
+my.lines <- c("I", "Iove", "You")
+typeof(my.lines)
+
 # Write a function `CompareLength` that takes in 2 vectors, and returns the sentence:
 # "The difference in lengths is N"
-
+CompareLength <- function(vector1, vector2) {
+  difference <- length(vector1) - length(vector2)
+  if (difference >= 0) {
+    return (paste("The difference in lengths is", difference))
+  } else {
+    return (paste("The difference in length is" + -difference))
+  }
+}
 
 # Pass two vectors of different length to your `CompareLength` function
 
